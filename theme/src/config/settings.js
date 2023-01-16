4d894032e5b688e40fdd431c970db9dd39999171
James Yang <jy0xcs@gmail.com>
2023-01-15 22:20:14 -0800

    Adjusted logo size.

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `X&J Products, Inc.`,
 	description: `Drive Systems | Motors | Gears`,
 	url: `https://www.xjproducts.com`, // No trailing slash!
-	name: `X&J`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `X&J Products, Inc.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
